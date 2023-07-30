b541fc4394cefacc9faffb5486326e261fc5b179
Cassio Izumi <izumicassio@gmail.com>
2023-07-30 14:32:15 -0300

    Added func getTotalArea() and modified requiedPanel array structure to number.

--- index.js
+++ index.js
@@ -12,45 +12,72 @@ Retorne:
 
 function getRequiredPanels(totalPower, panel) {
   let auxTotalPower = totalPower;
-  const requiredPanels = [];
+	//em caso de mais de um tipo de painel diferente podemos usar uma estrutura de array
+	let i = 0;
 
   while (auxTotalPower > 0) {
-    auxTotalPower = auxTotalPower - panel;
-    requiredPanels.push(availablePanelsArray[0]);
+    auxTotalPower = auxTotalPower - panel.powerInWatt;
+		i++
   }
 
-  return requiredPanels;
+  return i;
 }
 
-function getInvertersQty(requiredPanels) {
-  return Math.ceil(requiredPanels.length / 4);
+function getInvertersQty(requiredPanelsNum) {
+  return Math.ceil(requiredPanelsNum / 4);
 }
 
 function isNumber(value) {
   return !isNaN(value);
 }
 
-function getTotalLength(requiredPanels) {
-	const totalLength = requiredPanels.reduce((acc, currValue)=>acc+currValue.length,0).toFixed(2);
-	return parseFloat(totalLength);
+function getTotalLength(requiredPanelsNum, selectedPanel) {
+	const totalLength = requiredPanelsNum * selectedPanel.length;
+	return parseFloat(totalLength.toFixed(2));
 }
 
-function getSolarPanelInfos(totalPower, selectedPanel) {
-  if (isNaN(totalPower) || totalPower < 0) {
+function getTotalArea(totalLength, requiredPanelsNum, selectedPanel) {
+	const totalWidth = parseFloat((requiredPanelsNum * selectedPanel.length).toFixed(2));;
+	const totalArea = totalLength * totalWidth;
+
+	return parseFloat(totalArea.toFixed(2));
+}
+
+function getSolarPanelInfos(totalPowerInKiloWatts, selectedPanel) {
+  if (isNaN(totalPowerInKiloWatts) || totalPowerInKiloWatts < 0) {
     console.log(
-      "Valor de potência máxima inválida, por favor insira um valor numérico maior quem zero."
+      "Valor de potência máxima inválida, por favor insira um valor numérico maior que zero."
     );
     return;
   }
 
-  const requiredPanels = getRequiredPanels(totalPower, selectedPanel);
-  const invertersQty = getInvertersQty(requiredPanels);
-  const totalLength = getTotalLength();
-  const totalArea = getTotalArea();
+	const totalPowerInWatts = totalPowerInKiloWatts * 1000;
+
+  const requiredPanelsQty = getRequiredPanels(totalPowerInWatts, selectedPanel);
+  const invertersQty = getInvertersQty(requiredPanelsQty, selectedPanel);
+  const totalLength = getTotalLength(requiredPanelsQty, selectedPanel);
+  const totalArea = getTotalArea(totalLength, requiredPanelsQty, selectedPanel);
+
+	const solarPanelInfos = {
+		requiredPanelsQty,
+		invertersQty,
+		totalLength,
+		totalArea
+	}
+
+	const message = `Segundo os dados fornecidos para a potência total ${totalPowerInKiloWatts} em kW e usando painéis de ${selectedPanel.powerInWatt}w 
+	com as medidas ${selectedPanel.length}X${selectedPanel.width}. Serão necessários um total de ${requiredPanelsQty} panéis solares, ${invertersQty} inversores, 
+	um comprimeto de estrutura de ${totalLength}m e uma área útil de ${totalArea}m².`
+
+	console.log(message);
+
+	return solarPanelInfos;
 }
 
 const selectedPanel = {
   powerInWatt: 550,
   length: 1.95,
-  with: 1.1,
+  width: 1.1,
 };
+
+getSolarPanelInfos(4.5,selectedPanel);
