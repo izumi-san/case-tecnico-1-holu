a5f6c4acc37ac6947c04bf28a1f3856251524aee
Cassio Izumi <izumicassio@gmail.com>
2023-07-30 12:12:52 -0300

    Modified getSolarPanelInfos() to recieve panel object.

--- index.js
+++ index.js
@@ -9,45 +9,43 @@ Retorne:
  - Área Útil (metros quadrados);
 
 */
- 
-function getRequiredPanels(totalPower, availablePanelsArray) {
+
+function getRequiredPanels(totalPower, panel) {
   let auxTotalPower = totalPower;
-	const requiredPanels = []
-	while(auxTotalPower > 0) {
-		//Aqui vai ser a validação para escolher o painel mais adequado
-		auxTotalPower = auxTotalPower - availablePanelsArray[0].powerInWatt;
-		requiredPanels.push(availablePanelsArray[0]);
-	}
-	return requiredPanels;
+  const requiredPanels = [];
+
+  while (auxTotalPower > 0) {
+    auxTotalPower = auxTotalPower - panel;
+    requiredPanels.push(availablePanelsArray[0]);
+  }
+
+  return requiredPanels;
 }
 
-function getInvertersQty(requiredPanels){
-	return Math.ceil(requiredPanels.length/4);
+function getInvertersQty(requiredPanels) {
+  return Math.ceil(requiredPanels.length / 4);
 }
 
 function isNumber(value) {
   return !isNaN(value);
 }
 
-function getSolarPanelInfos(totalPower) {
+function getSolarPanelInfos(totalPower, selectedPanel) {
+  if (isNaN(totalPower) || totalPower < 0) {
+    console.log(
+      "Valor de potência máxima inválida, por favor insira um valor numérico maior quem zero."
+    );
+    return;
+  }
 
-	if ( isNaN(totalPower) || totalPower < 0 ) {
-		console.log("Valor de potência máxima inválida, por favor insira um valor numérico maior quem zero.");
-		return;
-	}
-
-  // Verificar se precisamos de mais de um tipo de painel
-	const availablePanelsArray = [
-		{
-			powerInWatt: 550,
-			length: 1.95,
-			with: 1.1
-		}
-	];
-
-  const requiredPanels = getRequiredPanels(totalPower, availablePanelsArray);
-	const invertersQty = getInvertersQty(requiredPanels);
-	const totalLength = getTotalLength();
-	const totalArea = getTotalArea();
+  const requiredPanels = getRequiredPanels(totalPower, selectedPanel);
+  const invertersQty = getInvertersQty(requiredPanels);
+  const totalLength = getTotalLength();
+  const totalArea = getTotalArea();
 }
 
+const selectedPanel = {
+  powerInWatt: 550,
+  length: 1.95,
+  with: 1.1,
+};
